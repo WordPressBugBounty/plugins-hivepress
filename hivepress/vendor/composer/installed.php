4d5ed28fb8ea6a66c809f41bb5a6eeff3f3a6e06
Sean Murphy <sean@iamseanmurphy.com>
2026-07-02 18:45:05 +0000

    1.7.26

--- hivepress/vendor/composer/installed.php
+++ hivepress/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.7.25',
-        'version' => '1.7.25.0',
-        'reference' => '748f53250d882c1b7ce47dc51f80acea9b711bab',
+        'pretty_version' => '1.7.26',
+        'version' => '1.7.26.0',
+        'reference' => '9350a63c73fd43a800c3bd5f69f8715917128dba',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.7.25',
-            'version' => '1.7.25.0',
-            'reference' => '748f53250d882c1b7ce47dc51f80acea9b711bab',
+            'pretty_version' => '1.7.26',
+            'version' => '1.7.26.0',
+            'reference' => '9350a63c73fd43a800c3bd5f69f8715917128dba',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
